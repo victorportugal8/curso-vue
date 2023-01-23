d889d276efcb3e624ef9b727890ade434182374e
Victor Portugal <victorportugalnobrega@gmail.com>
2023-01-23 20:03:35 -0300

    Adicionando a pasta formulario

---
+++ formulario/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <formulario/>
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
---
+++ formulario/src/components/InputEstrelas.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <i v-for="estrela in estrelas" :key="estrela.id" :class="estrela.estilo" @click="marcarAvaliacao(estrela.id)"></i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputEstrelas',
+    props: {
+        numeroEstrelas: {
+            type: Number,
+            required: true
+        }
+    },
+    data: () =>({
+        estrelas: [],
+        avaliacao: 0
+    }),
+    created(){
+        this.iniciarEstrelas()
+    },
+    methods: {
+        iniciarEstrelas(){
+            for(let i = 0; i < this.numeroEstrelas; i++){
+                this.estrelas[i] = {id: i, estilo: 'bi-star estrela'}
+            }
+        },
+        marcarAvaliacao(e){
+            this.iniciarEstrelas()
+            this.avaliacao = e + 1
+            
+            for(let i = 0; i < this.avaliacao; i++){
+                this.estrelas[i].estilo = 'bi-star-fill estrela preenchida'
+            }
+
+            // this.$emit('avaliar', this.avaliacao)
+            this.$emit('update:avaliar', this.avaliacao)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .estrela{
+        font-size: 1.5rem;
+        color: #999;
+        margin-left: 1px;
+        margin-right: 1px;
+    }
+    .preenchida{
+        color: #FC0;
+    }
+</style>
